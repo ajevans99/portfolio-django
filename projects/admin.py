b35fcfedb1cea02506ded0afc5fe8363a0e49293
Austin Evans <evansau1@msu.edu>
2019-03-25 01:35:31 -0400

    Project list enhancements

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from .models import Project
+from .models import Project, Tag
 
-@admin.register(Project)
-class ProjectAdmin(MarkdownxModelAdmin):
-    list_display = ('name',)
-    fields = ['name', 'description', 'video_link']
+# @admin.register(Project)
+# class ProjectAdmin(MarkdownxModelAdmin):
+#     list_display = ('name',)
+#     fields = ['name', 'description', 'video_link']
+admin.site.register(Project, MarkdownxModelAdmin)
+admin.site.register(Tag)
--- projects/models.py
+++ projects/models.py
@@ -3,14 +3,33 @@ from markdownx.models import MarkdownxField
 from django.urls import reverse
 from markdownx.utils import markdownify
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=6, verbose_name="Hex code")
+
+    def __str__(self):
+        return self.name
 class Project(models.Model):
     name = models.CharField(max_length=50)
+    date = models.DateField()
     description = MarkdownxField()
-    video_link = models.CharField(max_length=50, null=True)
+    video_link = models.CharField(max_length=200, null=True, blank=True)
+    preview_image_link = models.CharField(max_length=200, null=True, blank=True)
+    preview_description = MarkdownxField(null=True, blank=True)
+    github_link = models.CharField(max_length=200, null=True, blank=True)
+    external_link = models.CharField(max_length=200, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, verbose_name=("list of tags"), blank=True)
+
+    class Meta:
+        ordering = ('date', )
 
     @property
-    def formatted_markdown(self):
+    def formatted_description_markdown(self):
         return markdownify(self.description)
+
+    @property
+    def formatted_preview_markdown(self):
+        return markdownify(self.preview_description)
 
     def get_absolute_url(self):
         """
